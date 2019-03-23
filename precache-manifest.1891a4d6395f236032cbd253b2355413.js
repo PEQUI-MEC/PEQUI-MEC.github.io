3e2127405394a4cbc59371f8526007f2e84a3891
Thais Pires <tmendespires@gmail.com>
2019-03-22 22:22:47 -0300

    Updates

---
+++ precache-manifest.1891a4d6395f236032cbd253b2355413.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "117f6dc9d05ae6e70aa4",
+    "url": "/static/css/main.7f5d928f.chunk.css"
+  },
+  {
+    "revision": "117f6dc9d05ae6e70aa4",
+    "url": "/static/js/main.082118d8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a6f44752b8afe9a8348",
+    "url": "/static/js/2.8c8494ce.chunk.js"
+  },
+  {
+    "revision": "fb976e417efef38db525cd115b7b940d",
+    "url": "/index.html"
+  }
+];
