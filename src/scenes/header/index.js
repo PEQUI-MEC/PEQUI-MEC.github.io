695e8aca9c58b4a7d8c3bff7a700eec1bd0a3a41
guilherme <guilhermeferreir4@gmail.com>
2019-05-25 13:31:31 -0300

    ### Exemplo aprimorado para manter o mesmo idioma.
    - Ao clicar no botão idioma, é alternado entre português e inglês
    -- apenas para exemplificar.
    --TODO: definir como será realmente implementado

--- src/scenes/header/index.js
+++ src/scenes/header/index.js
@@ -9,24 +9,32 @@ class Header extends Component {
   render(){
     const lang = getSceneLanguage(this.props);
     // const {path} = this.props.location.path;
-    // const {location:{pathname}} = this.props;
+    const {location:{pathname}} = this.props;
     return (
       <div className='header-main'>
-        <NavLink to="/" style={{marginRight: 20}}>
+        <NavLink to={this.getToProps("/",lang)} style={{marginRight: 20}}>
           <img
             src={brand}
             height="35"
             alt="Pequi Mecânico"
           />
         </NavLink>
-        <NavLink to="/about" className='header-links'>{getLanguageText("ABOUT_US",lang)}</NavLink>
-        <NavLink to="/teams" className='header-links'>{getLanguageText("TEAMS",lang)}</NavLink>
-        <NavLink to="/contact" className='header-links'>{getLanguageText("CONTACT",lang)}</NavLink>
+        <NavLink to={this.getToProps("/about", lang)} className='header-links'>{getLanguageText("ABOUT_US",lang)}</NavLink>
+        <NavLink to={this.getToProps("/teams", lang)} className='header-links'>{getLanguageText("TEAMS",lang)}</NavLink>
+        <NavLink to={this.getToProps("/contact", lang)} className='header-links'>{getLanguageText("CONTACT",lang)}</NavLink>
         <div style={{flex: 1}}/>
-        <NavLink to={'/'} className='header-links'>{getLanguageText("LANGUAGE",lang)}</NavLink>
+        <NavLink to={this.getToProps(pathname, ['en','pt'].find(v => v !== lang))} className='header-links'>{getLanguageText("LANGUAGE",lang)}</NavLink>
       </div>
     )
   }
+
+  getToProps(path, lang){
+      return {
+          pathname: path,
+          search: `lang=${lang}`
+      };
+  };
+
 }
 
 export default withRouter(Header);
